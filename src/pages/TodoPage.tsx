494351afd3949b1c4c62bcdf51c9c10ac11cbe09
viethoangkxtb <viethoangkxtb@gmail.com>
2025-09-23 12:00:16 +0700

    Done

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -4,8 +4,8 @@ import TodoFooter from "../components/TodoFooter";
 
 export default function TodoPage() {
   return (
-    <div className="flex min-h-screen bg-[#f5f5f5]">
-      <div className="relative top-[90px] w-[550px] shadow-lg bg-[#f5f5f5]">
+    <div className="flex min-h-screen bg-[#f5f5f5] justify-center items-center">
+      <div className="relative w-[550px] shadow-lg bg-[#f5f5f5] mb-[50px]">
         <h1 className="text-[100px] text-center text-[#b83f45] mb-6 font-normal">
           todos
         </h1>
